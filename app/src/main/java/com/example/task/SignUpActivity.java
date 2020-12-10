271156ba6a8e90b5f9d1aeef68be871bcdceba5d
DELL <guptatanisha229@gmail.com>
2020-12-11 00:09:23 +0530

    hello

---
+++ app/src/main/java/com/example/task/SignUpActivity.java
@@ -0,0 +1,73 @@
+package com.example.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class SignUpActivity extends AppCompatActivity {
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+    TextView dob;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        dob = findViewById(R.id.dob);
+        signUp = findViewById(R.id.signUp);
+        radioGroup = findViewById(R.id.radioGroup);
+        myLister();
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(SignUpActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+            }
+        });
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = month + "/" + dayOfMonth + "/" + year;
+                dob.setText(date);
+            }
+        };
+    }
+    public void myLister(){
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int rgs_id = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(rgs_id);
+
+            }
+        });
+    }
+}
